673750a9d8da0b08c8a2db4ee0b6a180ff5b8ad4
MohamedSultan1981 <50361347+MohamedSultan1981@users.noreply.github.com>
2022-11-04 10:20:57 +0200

    v

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -41,7 +41,7 @@ def dashboard(request):
         #actions = actions.filter(user_id__in=following_ids)
         actions = actions.select_related('user', 'user__profile').prefetch_related('target')[:10] #for better performance 
         actions = actions[:10]
-        return render(request,'account/dashboard.html', {'section': 'dashboard','actions': actions})
+    return render(request,'account/dashboard.html', {'section': 'dashboard','actions': actions})
 def register(request):
     if request.method == 'POST':
         user_form = UserRegistrationForm(request.POST)
